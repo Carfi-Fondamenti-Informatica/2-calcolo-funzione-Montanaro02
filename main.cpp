2167554450c7688503ad514ed6ddb14318bd8ae5
Montanaro02 <100145766+Montanaro02@users.noreply.github.com>
2022-03-29 17:01:17 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 int main() {
-   float a=0, b=0, x=0, y=0;
-   cin>>a>>b>>x>>y;
-   int z=0;
-   if(x<0&&y>0){
-   }if(x>=0 && y<=0){
-       z=(a*(x*x))-(b*y);
+   float a=0, b=0, x=0, y=0, z=0;
+   cin >>a>>b>>x>>y;
+   if(x<0 & y>0){
+       z=a*x-b*y;
+   }if (x>=0 & y<=0){
+       z=a*(x*x)-b*y;
    }else{
-        z=(a*x)+(b*(y*y));
+        z=a*x+b*(y*y);
     }
    cout <<z;
    return 0;
