17863159f52735ba318c4407413bb2e234db2eca
Montanaro02 <100145766+Montanaro02@users.noreply.github.com>
2022-03-29 16:33:34 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
-
 int main() {
+   int a=0, b=0, x=0, y=0;
+   cout << "Inserire questi valori:";
+   cin>>a>>b>>x>>y;
+   int z;
+   if(x<0&&y>0){
+       z=(a*x)-(b*y);
+   }if(x>=0 && y<=0){
+       z=(a*(x*x))-(b*y);
+   }else{ 
+        z=(a*x)+(b*(y*y));
+    }
+   cout <<z;
    return 0;
 }
