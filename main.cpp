421a08754169d027c0c655d2041152b1094ec827
Montanaro02 <100145766+Montanaro02@users.noreply.github.com>
2022-03-29 17:17:02 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,9 +5,9 @@ int main() {
    float a=0, b=0, x=0, y=0;
    cin >>a>>b>>x>>y;
    float z=0;
-   if(x<0 & y>0){
+   if(x<0 && y>0){
        z=a*x-b*y;
-   }if (x>=0 & y<=0){
+   }else if (x>=0 && y<=0){
        z=a*(x*x)-b*y;
    }else{
        z=a*x+(b*(y*y));
